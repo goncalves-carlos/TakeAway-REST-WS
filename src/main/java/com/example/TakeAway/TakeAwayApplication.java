73bc02f26085c5591b455726508e23afacaa92dc
carlos <carlos.goncalves.002@student.uni.lu>
2021-12-28 15:48:38 +0100

    Inital Commit

---
+++ src/main/java/com/example/TakeAway/TakeAwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.TakeAway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeAwayApplication.class, args);
+	}
+
+}
