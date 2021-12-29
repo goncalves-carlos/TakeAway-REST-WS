4ca5b167ccc119a2afed7913cdeaf824f68ac37c
carlos <carlos.goncalves.002@student.uni.lu>
2021-12-29 01:27:02 +0100

    Added CRUD operations with respective html templates.

--- src/main/java/com/example/TakeAway/controller/TakeAwayController.java
+++ src/main/java/com/example/TakeAway/controller/TakeAwayController.java
@@ -3,15 +3,60 @@ package com.example.TakeAway.controller;
 import com.example.TakeAway.model.Dish;
 import com.example.TakeAway.repository.DishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-@RestController
+@Controller
 public class TakeAwayController {
     @Autowired
     DishRepository repository;
 
+    @GetMapping("/dishes")
+    public String listDishes(Model model) {
+        model.addAttribute("dishes", repository.findAll());
+        return "dishes";
+    }
+
+    @GetMapping("/dishes/new")
+    public String createDishForm(Model model) {
+        Dish dish = new Dish();
+        model.addAttribute("dish", dish);
+        return "create_dish";
+    }
+
+    @PostMapping("/dishes")
+    public String saveDish(@ModelAttribute("dish") Dish dish) {
+        repository.save(dish);
+        return "redirect:/dishes";
+    }
+
+    @GetMapping("/dishes/edit/{id}")
+    public String editDishForm(@PathVariable String id, Model model) {
+        model.addAttribute("dish", repository.findById(id).get());
+        return "edit_dish";
+    }
+
+    @PostMapping("/dishes/{id}")
+    public String updateDish(@PathVariable String id, @ModelAttribute("dish") Dish dish) {
+        Dish existingDish = repository.findById(id).get();
+        existingDish.setName(dish.getName());
+        existingDish.setCategories(dish.getCategories());
+        existingDish.setPrice(dish.getPrice());
+        repository.save(existingDish);
+        return "redirect:/dishes";
+    }
+
+    @GetMapping("/dishes/{id}")
+    public String deleteDish(@PathVariable String id) {
+        repository.deleteById(id);
+        return "redirect:/dishes";
+    }
+
+    /*
     @PostMapping("/addDish")
     public String saveDish(@RequestBody Dish dish) {
         repository.save(dish);
@@ -28,4 +73,6 @@ public class TakeAwayController {
         repository.deleteById(id);
         return "Dish deleted with id: " + id;
     }
+
+     */
 }
